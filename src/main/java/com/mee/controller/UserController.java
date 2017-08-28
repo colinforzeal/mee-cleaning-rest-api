d8e8797d7d62bc49df1168f459f2b6b2cffd2a02
Uladzislau Bondar <colinforzeal@yandex.ru>
2017-08-28 18:40:18 +0300

    added dull user controller

---
+++ src/main/java/com/mee/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.mee.controller;
+
+import com.mee.entity.User;
+import com.mee.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping("/users")
+    public User findAll() {
+        return userRepository.findByFirstName("hello");
+    }
+}
